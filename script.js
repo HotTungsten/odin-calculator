deb98755f9b3274aa526cd1abad49f46c9e639d1
unknown <hottungsten@gmail.com>
2022-04-21 00:21:48 -0700

    number button functionality

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ function operate(operator, num1, num2){
             return num1 / num2;
     }
 }
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach(numbers => numbers.addEventListener('click', function(e){
+
+    console.log(numbers.innerHTML);
+    console.log(typeof numbers.innerHTML);
+}));
