1d069b63c244971d8edd0a7788ff197b50fbe91d
unknown <hottungsten@gmail.com>
2022-04-23 23:50:42 -0700

    math framework for js

--- script.js
+++ script.js
@@ -12,8 +12,37 @@ function operate(operator, num1, num2){
 }
 
 const numbers = document.querySelectorAll('.number');
-numbers.forEach(numbers => numbers.addEventListener('click', function(e){
+let numOne = "";
+let numTwo = "";
+let operator = false;
+let operation = "";
 
-    console.log(numbers.innerHTML);
-    console.log(typeof numbers.innerHTML);
+numbers.forEach(numbers => numbers.addEventListener('click', function(){
+    if(operator === false){
+        numOne += numbers.innerHTML;
+        console.log(numOne);
+    }
+    else {
+        numTwo += numbers.innerHTML;
+        console.log(numTwo);
+    }
+}));
+
+const clear = document.querySelectorAll('#clear');
+clear.forEach(clear => clear.addEventListener('click', function(){
+    numOne = "";
+    numTwo = "";
+    operator = false;
+    operation = "";
+}));
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operators => operators.addEventListener('click', function(){
+    if(operator){
+        
+    }
+    else{
+        operation = operators.innerHTML
+        operator = true;
+    }
 }));
