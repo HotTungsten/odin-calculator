1d05220fc972a68d44efe918431433b6ca0f64d5
unknown <hottungsten@gmail.com>
2022-04-25 00:00:14 -0700

    added operation functionality

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-function operate(operator, num1, num2){
-    switch(operator){
-        case "+":
-            return num1 + num2;
-        case "-":
-            return num1 - num2;
-        case "*":
-            return num1 * num2;
-        case "/":
-            return num1 / num2;
-    }
-}
-
 const numbers = document.querySelectorAll('.number');
-let numOne = "";
-let numTwo = "";
+let numOne = "0";
+let numTwo = "0";
 let operator = false;
 let operation = "";
 
@@ -30,8 +17,8 @@ numbers.forEach(numbers => numbers.addEventListener('click', function(){
 
 const clear = document.querySelectorAll('#clear');
 clear.forEach(clear => clear.addEventListener('click', function(){
-    numOne = "";
-    numTwo = "";
+    numOne = "0";
+    numTwo = "0";
     operator = false;
     operation = "";
 }));
@@ -45,4 +32,29 @@ operators.forEach(operators => operators.addEventListener('click', function(){
         operation = operators.innerHTML
         operator = true;
     }
+    console.log(operation);
 }));
+
+const equals = document.querySelectorAll('#equals');
+equals.forEach(equals => equals.addEventListener('click', function(){
+    if(numOne !== "0" && numTwo !== "0" && operation !== ""){
+        console.log(operate(operator, parseInt(numOne), parseInt(numTwo)));
+    }
+}));
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            return num1 / num2;
+    }
+}
+
+function clearAll(){
+    
+}
